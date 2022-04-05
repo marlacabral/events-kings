235af6df6d034b9c86ae911ad0aa4288d7134bae
marlacabral <roncolicabral@gmail.com>
2022-04-05 19:15:22 -0300

    .

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const cors = require("cors")
+const port = 3001;
+
+const app = express();
+app.use(cors());
+
+app.use(express.json());
+
+const staff = [
+    {
+        id: 1,
+        name: "Marla Cabral",
+        idade: "32",
+        email: "devmarlacabral@gmail.com",
+        whatsapp: "51996140140",
+        fone: "7398517608",
+        rg: "8093744012",
+        cpf: "01361186070",
+        experienceEvents: "Garçonete, bar, caixa",
+        pic: "./assets/img1.png",
+        adress: "Rua Dorval Marques, 166 Santa Teresa Porto Alegre/RS"
+    },
+    {
+        id: 2,
+        name: "José Júnior",
+        idade: "22",
+        email: "jose@gmail.com",
+        whatsapp: "51994140242",
+        fone: "51994140242",
+        rg: "5096276265",
+        cpf: "04069158065",
+        experienceEvents: "Bar",
+        pic: "./assets/img2.png",
+        adress: "Rua A, 100 Porto Alegre/RS"
+    }
+];
+
+app.get('/', (req, res) => {
+    res.send(staff);
+})
+
+app.get('/staff', (req, res) => {
+    res.send(staff);
+})
+
+app.get('/staff/find/(:id)?', (req, res) => {
+    const idParam = req.params.id;
+    const unicColaborador = staff.find((colaborador) => colaborador.id == idParam);
+    console.log(unicColaborador);
+    if(unicColaborador === undefined){
+        res.send({message: "Nenhum colaborador encontrado"});
+    }
+    res.send(unicColaborador);
+});
+
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`)
+});
+
+
---
+++ script.js
@@ -0,0 +1,54 @@
+const baseUrl = "http://localhost:3001"
+
+const findStaff= async () => {
+    const response = await fetch(`${baseUrl}/staff`);
+    const colaborador = await response.json();
+    colaborador.forEach((colaborador) => {
+        document.getElementById("staffList").insertAdjacentHTML(
+            "beforeend",
+            `<div class="StaffListaItem">
+            <div>
+                <div class="StaffListaItem_name">${`Nome: ${staff.name}`}</div>
+                <div class="StaffListaItem_idade">${`Idade: ${staff.idade}`}</div>
+                <div class="StaffListaItem_email">${`Email: ${staff.email}`}</div>
+                <div class="StaffListaItem_whatsapp">${`Whatsapp: ${staff.whatsapp}`}</div>
+                <div class="StaffListaItem_fone">${`Fone: ${staff.fone}`}</div>
+                <div class="StaffListaItem_rg">${`RG: ${staff.rg}`}</div>
+                <div class="StaffListaItem_cpf">${`CPF: ${staff.cpf}`}</div>
+                <div class="StaffListaItem_experienceEvents">${`Experiencia com Eventos: ${staff.experienceEvents}`}</div>
+                <div class="StaffListaItem_adress">${`Endereço: ${staff.adress}`}</div>
+            </div>
+            <img class="StaffListaItem_pic" src=${staff.pic} alt=${`${staff.name}`}/>
+            </div>`,
+        );
+    });
+};
+
+const findByIdColaboradores = async () => {
+    const id = document.getElementById("idStaff").value;
+
+    const response = await fetch(`${baseUrl}/staff/find/${id}`);
+    
+    const staff = await response.json();
+
+    const colaboradorEncolhidoDiv = document.getElementById("staffEscolhido");
+
+    colaboradorEncolhidoDiv.innerHTML = `
+    <div class ="StaffCardItem">
+        <div>
+            <div class="StaffListaItem_name">${`Nome: ${staff.name}`}</div>
+            <div class="StaffListaItem_idade">${`Idade: ${staff.idade}`}</div>
+            <div class="StaffListaItem_email">${`Email: ${staff.email}`}</div>
+            <div class="StaffListaItem_whatsapp">${`Whatsapp: ${staff.whatsapp}`}</div>
+            <div class="StaffListaItem_fone">${`Fone: ${staff.fone}`}</div>
+            <div class="StaffListaItem_rg">${`RG: ${staff.rg}`}</div>
+            <div class="StaffListaItem_cpf">${`CPF: ${staff.cpf}`}</div>
+            <div class="StaffListaItem_experienceEvents">${`Experiencia com Eventos: ${staff.experienceEvents}`}</div>
+            <div class="StaffListaItem_adress">${`Endereço: ${staff.adress}`}</div>
+        </div>
+            <img class="StaffListaItem_pic" src=${staff.pic} alt=${`${staff.name}`}/>
+    </div>
+    `;
+};
+
+findStaff();
